32aa7933aa61f2c2d46c6d8085c7a6f43b7cecdc
Manish Sharma <81001090+manishsharma864@users.noreply.github.com>
2026-04-09 11:09:13 +0530

    Update page.tsx
    changed to real prod url

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -8,7 +8,7 @@ import {
 } from "lucide-react";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://reality-check-mcp-8eqi.onrender.com/api";
 
 export default function Home() {
   const [idea, setIdea] = useState("");
